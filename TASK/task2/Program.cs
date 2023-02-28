be080068e134bdc1f8a539cb5b1546247bbca4c3
Stepan <sud_pravo@bk.ru>
2023-02-28 10:33:40 +0700

    tsk2

---
+++ TASK/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 27: Напишите метод, который 
+принимает на вход число и выдаёт 
+сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int Promt(string text)
+{
+    Console.Write(text);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int SumDigit(int num)
+{
+    int result = 0;
+    while(num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+int number = Promt("Введите число ");
+Console.WriteLine($"Сумма всех цифр в числе {number} = {SumDigit(number)}");
