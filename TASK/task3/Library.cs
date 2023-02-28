958ace677a313fcdbfd749c1a2f2d6c355ebaa3c
Stepan <sud_pravo@bk.ru>
2023-02-28 12:55:07 +0700

    task3

--- TASK/task3/Library.cs
+++ TASK/task3/Library.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Task_1
+namespace task3
 {
     public class Library
     {
--- TASK/task3/Program.cs
+++ TASK/task3/Program.cs
@@ -7,3 +7,25 @@
 
 6, 1, 33 -> [6, 1, 33]
 */
+void WriteArray(int[] arr, int a, int b) 
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(a, b);
+    }
+}
+
+
+String PrintArray(int[] arr)
+{
+    string output = String.Empty;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        output += arr[i] + " ";
+    }
+    return output;
+}
+
+int[] array = new int[10];
+WriteArray(array, 1, 100);
+Console.WriteLine(PrintArray(array));
